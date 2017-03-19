2641d91828b05e9403b52090d4a42e0022034d41
rec16005 <rec16005@uvg.edu.gt>
2017-03-18 21:34:55 -0600

    assests commit2

---
+++ Assets/Scripts/scriptEnemigo.js
@@ -0,0 +1,36 @@
+﻿public var speed : int = -5;
+
+
+function Start () {
+  
+    var r2d = GetComponent("Rigidbody2D");
+
+  
+    r2d.velocity.y = speed;
+
+    
+    r2d.angularVelocity = Random.Range(-200, 200);
+}
+
+
+function OnBecameInvisible() {
+
+    Destroy(gameObject);
+} 
+
+function OnTriggerEnter2D(obj) {
+    
+    var name = obj.gameObject.name;
+
+    if (name == "bullet(Clone)") {
+       
+        Destroy(gameObject);
+        Destroy(obj.gameObject);
+    }
+
+    
+    if (name == "spaceship") {
+      
+        Destroy(gameObject);
+    }
+} 
---
+++ Assets/Scripts/scriptSpawner.js
@@ -0,0 +1,26 @@
+﻿public var enemy : GameObject;
+
+
+public var spawnTime : float = 2;
+
+function Start() {
+    
+    InvokeRepeating("addEnemy", 0, spawnTime);
+}
+
+
+function addEnemy() {
+   
+    var rd = GetComponent("Renderer");
+
+   
+    var x1 = transform.position.x - rd.bounds.size.x/2;
+
+    var x2 = transform.position.x + rd.bounds.size.x/2;
+
+
+    var spawnPoint = Vector2(Random.Range(x1, x2), transform.position.y);
+
+   
+    Instantiate(enemy, spawnPoint, Quaternion.identity);
+} 
