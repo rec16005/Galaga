3b5fe54e7b57b252e32d3a7c0ca2b9f1ee73160e
rec16005 <rec16005@uvg.edu.gt>
2017-03-18 20:29:32 -0600

    balas funcionan

---
+++ Assets/Scripts/scriptBala.js
@@ -0,0 +1,17 @@
+﻿
+public var speed : int = 6;
+
+
+function Start () {
+    
+    var r2d = GetComponent("Rigidbody2D");
+
+  
+    r2d.velocity.y = speed;
+}
+
+
+function OnBecameInvisible() {
+    
+    Destroy(gameObject);
+} 
---
+++ Assets/Scripts/scriptNave.js
@@ -0,0 +1,21 @@
+﻿
+public var bullet : GameObject;
+function Start () {
+	
+}
+
+function Update () {
+
+    var r2d = GetComponent("Rigidbody2D");
+
+    if (Input.GetKey("right"))
+        r2d.velocity.x = 10;
+    else if (Input.GetKey("left"))
+        r2d.velocity.x = -10;
+    else
+        r2d.velocity.x = 0;
+	
+    if (Input.GetKeyDown("space")) {
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+}
