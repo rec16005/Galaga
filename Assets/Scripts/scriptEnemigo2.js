09b1145d42846d4e5a8dfa4222af286cba2e13c4
rec16005 <rec16005@uvg.edu.gt>
2017-03-19 17:02:15 -0600

    enemigos estaticos

---
+++ Assets/Scripts/scriptEnemigo2.js
@@ -0,0 +1,32 @@
+﻿public var speed : int = 3;
+
+
+function Start () {
+  
+    var r2d = GetComponent("Rigidbody2D");
+    
+    r2d.angularVelocity = Random.Range(-200, 200);
+}
+
+
+function OnBecameInvisible() {
+
+    Destroy(gameObject);
+} 
+
+function OnTriggerEnter2D(obj) {
+    
+    var name = obj.gameObject.name;
+
+    if (name == "bullet(Clone)") {
+       
+        Destroy(gameObject);
+        Destroy(obj.gameObject);
+    }
+
+    
+    if (name == "spaceship") {
+      
+        Destroy(gameObject);
+    }
+} 
